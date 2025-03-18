159e75d4db17674ccc3b1e32b52b8b3ce0d565d9
paprisko <jackmchussein965@gmail.com>
2025-03-18 03:06:29 +0300

    phase-1-stitching-together-the-three-pillars

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+const articleHearts = document.querySelectorAll(".like-glyph");
+console.log(articleHearts); 
+
